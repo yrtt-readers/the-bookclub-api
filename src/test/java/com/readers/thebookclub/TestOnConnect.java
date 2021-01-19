b4eb13bd7c2baa8ccf76162a33b154c56e83844a
Winndie Fan <winndie@gmail.com>
2021-01-19 18:19:55 +0000

    1st test Response

--- src/test/java/com/readers/thebookclub/TestOnConnect.java
+++ src/test/java/com/readers/thebookclub/TestOnConnect.java
@@ -2,11 +2,15 @@ package com.readers.thebookclub;
 
 import org.junit.Test;
 import static org.junit.Assert.assertEquals;
+import java.util.Map;
+import java.util.HashMap;
  
 public class TestOnConnect {
  
     @Test
     public void checkConnection() {
-        assertEquals("Hello","Hello"); 
+        Map<String, Object> input = new HashMap<String, Object>();
+        Response r = new Response("Hello World",input);
+        assertEquals("Hello World","Hello World");
     }
 }

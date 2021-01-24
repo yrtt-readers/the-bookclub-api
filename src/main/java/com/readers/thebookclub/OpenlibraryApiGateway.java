e8a5d1425ca927a43aaa1c0a54f95c56d307c008
Winndie Fan <winndie@gmail.com>
2021-01-24 12:33:55 +0000

    fix java.net.http not exists

--- src/main/java/com/readers/thebookclub/OpenlibraryApiGateway.java
+++ src/main/java/com/readers/thebookclub/OpenlibraryApiGateway.java
@@ -4,12 +4,13 @@ import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.io.BufferedReader;
 
+import java.net.URL;
+import java.net.URI;
+
 import java.net.http.HttpClient;
 import java.net.http.HttpRequest;
 import java.net.http.HttpResponse;
 import java.net.http.HttpResponse.BodyHandlers;
-import java.net.URL;
-import java.net.URI;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import com.fasterxml.jackson.databind.JsonNode;
